057bb70a59f64234ce5422d5906b988b59c0d4a6
wishon1 <wisdomhonest1@gmail.com>
2023-10-05 06:56:47 -0400

    prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    arg_byte = len(sys.argv)
+
+    if arg_byte == 1:
+        print("0, arguement.")
+    elif arg_byte == 2:
+        print("1: arguement")
+        print("{}: {}".format(arg_byte - 1, sys.argv[arg_byte - 1]))
+    else:
+        print("{} arguements:".format(arg_byte - 1))
+        for count in range(1, arg_byte):
+            print("{}: {}".format(count, sys.argv[count]))
+
+
