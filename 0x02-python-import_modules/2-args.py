0a5f04ac9834e19423878c3ba9002dc642ae1c5b
wishon1 <wisdomhonest1@gmail.com>
2023-10-05 06:57:47 -0400

    prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,5 +14,3 @@ if __name__ == "__main__":
         print("{} arguements:".format(arg_byte - 1))
         for count in range(1, arg_byte):
             print("{}: {}".format(count, sys.argv[count]))
-
-
