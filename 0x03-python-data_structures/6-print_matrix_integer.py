e2e46588d1ea27d8b19c2b19039a3425f9938581
wishon1 <wisdomhonest1@gmail.com>
2023-10-07 17:29:13 -0400

    add update to 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+function that prints a matrix of integers.
+"""
+
+def print_matrix_integer(matrix=[[]]):
+    for count in matrix:
+        for index in count:
+
+            
