bebec2c05a0e47c7bbbcb9f981128d45e792427e
wishon1 <wisdomhonest1@gmail.com>
2023-10-25 15:25:18 -0400

    empty class Square that defines a square:

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""
+This class does not have an attribute
+"""
+
+
 class Square:
-    """
-    This class does not have an attribute
-    """
+    """ This class does not have an attribute """
     pass
