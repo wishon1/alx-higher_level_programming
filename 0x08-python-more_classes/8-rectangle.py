8965c76024d633b87c3d3ac1b5a1093efc36f83b
wishon1 <wisdomhonest1@gmail.com>
2023-10-30 16:29:41 -0400

    task 9

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -91,7 +91,7 @@ class Rectangle:
         '''Prints the message when an instance of Rectangle is deleted'''
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ returns the biggest rectangle based on the area """
