f182844a0df740e1da39c4970c16a1c59482dabc
wishon1 <wisdomhonest1@gmail.com>
2023-10-30 12:51:39 -0400

    task 4

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -17,8 +17,17 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ __init__ method to initialize the attributes of the class"""
-        self.width = width
-        self.height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
 
     @property
     def width(self):
