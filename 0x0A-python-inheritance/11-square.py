dcc03265e5c367991a0c2866d6a1aff065bf7b85
wishon1 <wisdomhonest1@gmail.com>
2023-11-06 16:01:02 -0500

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""
-    class Square that inherits from Rectangle (9-rectangle.py).
-    (task based on 10-square.py).
-"""
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Rep.resent a square"""
+    """ class Square """
 
     def __init__(self, size):
-        """Initialize a new square
         """
-        self.integer_validator("size", size)
+        Constructor __init__ - A constructor for instantiation.
+        Args:
+            size: The size of the square.
+        """
         super().__init__(size, size)
         self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        Returns:
+            The area of the square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        String representation of the square.
+        Returns:
+            A formatted string describing the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
