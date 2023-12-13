ef429b497a5713468535fb4d60f6a4c76666e877
wishon1 <wisdomhonest1@gmail.com>
2023-12-13 14:44:07 -0500

    update all

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,4 @@
 -- computes the score average of all records in the table second_table
+-- The result column name should be average
+-- The database name will be passed as an argument of the mysql command
 SELECT AVG(score) AS average FROM second_table;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,10 @@
 -- lists the number of records with the same score in the table second_table
+-- The result should display:
+-- the score
+-- the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
+-- The database name will be passed as an argument to the mysql command
+
 SELECT score, COUNT(*) AS number FROM second_table
 GROUP BY score
 ORDER BY score DESC;
