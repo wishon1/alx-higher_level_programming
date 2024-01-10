e4ae42ee427b38674174dce635f6e43de3b3a9d4
wishon1 <wisdomhonest1@gmail.com>
2024-01-10 10:19:45 -0500

    add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let fullWidth = '';
+    for (let i = 0; i < this.width; i++) {
+      fullWidth += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(fullWidth);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
