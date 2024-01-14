1ea89d1133ed99894db662df545e79ca689f0b84
wishon1 <wisdomhonest1@gmail.com>
2024-01-14 11:46:45 -0500

    ass file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     interact_db = db.cursor()
-    interact_db.execute("SELECT * FROM 'states' ORDER BY 'id'")
+    interact_db.execute("SELECT * FROM `states` ORDER BY `id`")
 
     for state in interact_db.fetchall():
         if state[1][0] == 'N':
