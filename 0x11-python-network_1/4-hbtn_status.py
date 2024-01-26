99db32164419293dcfcd3a5557d06208f5e44521
wishon1 <wisdomhonest1@gmail.com>
2024-01-26 11:49:31 -0500

    add 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+def fetch_data(data):
+    """scripts that fetches https://alx-intranet.hbtn.io/status"""
+    request = requests.get(data)
+    print("Body response")
+    print("\t - type: {}".format(type(request.text)))
+    print("\t - content: {}".format(request.text))
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_data(url)
