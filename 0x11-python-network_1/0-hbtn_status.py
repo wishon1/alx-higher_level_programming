6e7f192c1fd8fbc1445611abf7118422e9e1ba2e
wishon1 <wisdomhonest1@gmail.com>
2024-01-26 08:14:36 -0500

    add task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,7 +14,8 @@ def fetch_url():
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- -tf8 content: {}".format(content.decode('utf-8')))
-    
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+
 if __name__ == '__main__':
     fetch_url()
