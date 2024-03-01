f08419628443b46611b952ef31b91bf2d04c1508
wishon1 <wisdomhonest1@gmail.com>
2024-03-01 01:45:00 -0500

    add updates

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$('div#toggle_header').click(function() {
+  var header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red').addClass('green');
+  } else {
+    header.removeClass('green').addClass('red');
+  }
+});
